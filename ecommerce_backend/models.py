9c22cdd5673b9c501cd943a59fdae4d2da9a1242
Cecilia Aguilera <ceci.aguilera@zohomail.com>
2021-05-26 17:10:07 -0400

    Category model fixed

--- ecommerce_backend/models.py
+++ ecommerce_backend/models.py
@@ -90,6 +90,9 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural='categories'
 
+    def __str__(self):
+        return self.category_name
+
 
 
 # ==============================================================================
