4a368062a9587c0706e1aeb29598ca1aac5b690b
Ceci-Aguilera <ceci.aguilera@zohomail.com>
2021-05-30 03:01:50 -0400

    Created Check-Auth and Login/Out Views

--- ecommerce_accounts_app/urls.py
+++ ecommerce_accounts_app/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.conf.urls import url,include
-from .views import SignUpAPIView
+from .views import (
+    SignUpAPIView,
+    CheckAuthenticatedView,
+    LoginView,
+    LogoutView
+)
 
 app_name = 'ecommerce_accounts_app'
 
 urlpatterns = [
-    url(r'^sign-up$', SignUpAPIView, name='sign-up-api'),
+    url(r'^sign-up$', SignUpAPIView.as_view(), name='sign-up-api'),
+    url(r'^check-auth$', CheckAuthenticatedView.as_view(), name='check-auth-api'),
+    url(r'^login$', LoginView.as_view(), name='login-api'),
+    url(r'^logout$', LoginView.as_view(), name='logout-api'),
 ]
--- ecommerce_accounts_app/views.py
+++ ecommerce_accounts_app/views.py
@@ -2,58 +2,105 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
-# from django.utils.decorators import method_decorator
-# from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
 from rest_framework.decorators import api_view
 from rest_framework.decorators import authentication_classes, permission_classes
 from django.http import HttpResponse, JsonResponse
 import json
 from rest_framework.response import Response
 from rest_framework import status
+from django.contrib import auth
 
+from .models import User
 from .serializers import UserRegisterSerializer
 
 # Create your views here.
 
-# @method_decorator(ensure_csrf_cookie, name='dispatch')
-@api_view(['GET',])
-@ensure_csrf_cookie
+
 @permission_classes([AllowAny])
-def GetCSRFToken(request):
-    permissions_classes = [AllowAny, ]
+class GetCSRFToken(APIView):
 
-    if request.method == 'GET':
-        return Response({'success': 'CSRF cookie set'})
+    @method_decorator(ensure_csrf_cookie)
+    def get(self, request,*args, **kwargs):
+        return Response({'Success': 'CSRF cookie set'})
 
 
-@api_view(['POST',])
+
+class CheckAuthenticatedView(APIView):
+
+    @method_decorator(csrf_protect)
+    def get(self, request, format=None):
+        result = 'Success'
+        try:
+            isAuthenticated = User.is_authenticated
+            if isAuthenticated == False:
+                result='Error'
+        except:
+            result = 'Error'
+
+        return Response({'isAuthenticated':result})
+
+
+
+
 @permission_classes([AllowAny])
-@csrf_protect
-def SignUpAPIView(request):
-    permissions_classes = [AllowAny, ]
+class LoginView(APIView):
 
-    if request.method=='POST':
+    @method_decorator(csrf_protect)
+    def post(self, request, format=None):
 
-        # return Response("OK")
+        data = request.data
+        email = data['email']
+        password = data['password']
+        result = 'Error Authenticating'
+        try:
+            user = auth.authenticate(username=email,password=password)
+            if user is not None:
+                auth.login(request, user)
+                result = 'Success at Authenticating'
+        except:
+            pass
+
+        return Response({'Login Result':result})
+
+
+
+class LogoutView(APIView):
+
+    def post(self, request, format=None):
+        try:
+            auth.logout(request)
+            result = 'Success'
+        except:
+            result = 'Something went wrong when logging out'
+        return Response({'Logout Result':result})
+
+
+
+@permission_classes([AllowAny])
+class SignUpAPIView(APIView):
+
+    @method_decorator(csrf_protect)
+    def post(self, request,*args, **kwargs):
 
         data = request.data
         password = data['password']
         re_password = data['re_password']
+        result = 'User created'
+        status_result = status.HTTP_201_CREATED
 
         if password != re_password:
-            return Response(
-                {'error':'Password does not match'},
-                status=status.HTTP_400_BAD_REQUEST
-            )
+            result = 'Passwords do not match'
+            status_result = status.HTTP_400_BAD_REQUEST
 
-        user_serializer = UserRegisterSerializer(data=data)
-        if user_serializer.is_valid() == False:
-            return Response(
-                {'error': user_serializer.errors},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        user = user_serializer.save()
-        return Response(
-            {'status': 'User created'},
-            status=status.HTTP_201_CREATED
-        )
+        else:
+            user_serializer = UserRegisterSerializer(data=data)
+
+            if user_serializer.is_valid() == False:
+                result = user_serializer.errors
+                status_result = status.HTTP_400_BAD_REQUEST
+            else:
+                user = user_serializer.save()
+
+        return Response({'Register result': result}, status=status_result)
